9ad25929486105defdd294292fcf5c7ae0fc8459
Witali <witalihirsch1@gmail.com>
2026-03-15 14:40:20 +0500

    website

--- assets/framerusercontent.com/sites/3RVfJyYHC2u7wH6yIWm7S1/rolldown-runtime.V3gPc3Ui.mjs
+++
@@ -1,1 +0,0 @@
-var e=Object.defineProperty,t=(e,t)=>()=>(e&&(t=e(e=0)),t),n=(t,n)=>{let r={};for(var i in t)e(r,i,{get:t[i],enumerable:!0});return n&&e(r,Symbol.toStringTag,{value:`Module`}),r};export{n,t};
